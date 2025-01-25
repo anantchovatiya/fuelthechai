97d81e213a59973b14c9a09359a96d2949737412
anantchovatiya <147299670+anantchovatiya@users.noreply.github.com>
2025-01-25 19:14:08 +0530

    Update Footer.jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className='bg-black text-white px-5 h-16 flex justify-center items-center'>
-        <p className='text-center'>Copyright &copy; Get me a chai! - All rights reserved 2025.</p>
+        <p className='text-center'>Copyright &copy; 2025. Fuel the chai! - A project by Anant Chovatiya ❤️. All rights reserved.</p>
     </footer>
   )
 }
