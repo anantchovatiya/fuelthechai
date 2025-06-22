f03b6970965bce146647266203fe4cb60003c781
Anant Chovatiya <147299670+anantchovatiya@users.noreply.github.com>
2025-06-22 21:03:00 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
   } catch (error) {
     process.exit(1);
   }
